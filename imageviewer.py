6d6916355a7751156cba9e1e707184ab2288918f
sulei1324 <651343980@qq.com>
2015-05-26 22:31:10 +0800

    ok

---
+++ imageviewer.py
@@ -0,0 +1,16 @@
+__author__ = 'Su Lei'
+
+import pyglet
+
+window = pyglet.window.Window()
+image = pyglet.resource.image('test.jpg')
+
+
+@window.event
+def on_draw():
+    window.clear()
+    image.blit(0, 0)
+
+
+pyglet.app.run()
+

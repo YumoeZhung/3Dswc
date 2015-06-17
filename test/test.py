5f3647bcf49f84137270afb5e6320c1377a69f4e
sulei1324 <651343980@qq.com>
2015-06-17 14:45:43 +0800

    ok

--- test/test.py
+++ test/test.py
@@ -1,13 +1,10 @@
 __author__ = 'Su Lei'
 
-import numpy as np
+import re
 
-a = np.array([0, 245, 6745, 23112, 2 ** 16 -1], dtype=np.uint16)
-print a + 10
-b = np.zeros((1, 3), dtype=np.uint8)
-b = np.uint8(a)
-print b
-print (a / (2.0 ** 16 - 1)) * (2 ** 8 - 1)
-c = np.uint8((a / (2.0 ** 16 - 1)) * (2 ** 8 - 1))
-print c
-
+r = "[j-p]{1,2}"
+rex = re.compile(r)
+rex.match('ooooooooo')
+print rex.groups
+a = re.match(r, 'nn')
+print a.group()

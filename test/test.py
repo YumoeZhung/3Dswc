92c4a7580435eaca20da7f410dcaaa88cfe74219
sulei1324 <651343980@qq.com>
2015-07-09 18:55:47 +0800

    ok

--- test/test.py
+++ test/test.py
@@ -1,50 +1,59 @@
 import vtk
-from vtk import *
 
-#setup points and vertices
-Points = vtk.vtkPoints()
-Triangles = vtk.vtkCellArray()
-
-Points.InsertNextPoint(1.0, 0.0, 0.0)
-Points.InsertNextPoint(0.0, 0.0, 0.0)
-Points.InsertNextPoint(0.0, 1.0, 0.0)
-
-Triangle = vtk.vtkTriangle()
-Triangle.GetPointIds().SetId(0, 0)
-Triangle.GetPointIds().SetId(1, 1)
-Triangle.GetPointIds().SetId(2, 2)
-Triangles.InsertNextCell(Triangle)
-
-#setup colors
-Colors = vtk.vtkUnsignedCharArray()
-Colors.SetNumberOfComponents(3)
-Colors.SetName("Colors")
-Colors.InsertNextTuple3(255, 0, 0)
-Colors.InsertNextTuple3(0, 255, 0)
-Colors.InsertNextTuple3(0, 0, 255)
-
-polydata = vtk.vtkPolyData()
-polydata.SetPoints(Points)
-polydata.SetPolys(Triangles)
-
-polydata.GetPointData().SetScalars(Colors)
-
-
-mapper = vtk.vtkPolyDataMapper()
-mapper.SetInputData(polydata)
-
-#create the actor with the mapper
-actor = vtk.vtkActor()
-actor.SetMapper(mapper)
-
-ren = vtk.vtkRenderer()
+t1 = 0
+t2 = 0
+t3 = 0
 iren = vtk.vtkRenderWindowInteractor()
-renWin = vtk.vtkRenderWindow()
-#add the actor to the display
-ren.AddActor(actor)
-iren.SetRenderWindow(renWin)
 
-# enable user interface interactor
-iren.Initialize()
-renWin.Render()
-iren.Start()
+def pickerfunc (object, event):
+    if event == 'LeftButtonPressEvent':
+        print 'DOK'
+        print t1
+   
+    if event == 'LeftButtonReleaseEvent':
+        print 'ROK'
+        print t2
+
+    if event == 'RightButtonPressEvent':
+        print t3
+    iren.RemoveObserver (t1)
+    iren.RemoveObserver (t2)
+    iren.RemoveObserver (t3)
+   
+
+def main():
+    cone = vtk.vtkConeSource()
+    cone.SetHeight( 3.0 )
+    cone.SetRadius( 1.0 )
+    cone.SetResolution( 10 )
+
+    map = vtk.vtkPolyDataMapper()
+    map.SetInputConnection(cone.GetOutputPort())
+
+    actor = vtk.vtkActor()
+    actor.SetMapper(map)
+
+    ren = vtk.vtkRenderer()
+    ren.AddActor(actor)
+    ren.SetBackground(0.1, 0.2, 0.4)
+
+    renWin = vtk.vtkRenderWindow()
+    renWin.AddRenderer(ren)
+    renWin.SetSize(300, 300)
+
+    iren.SetRenderWindow(renWin)
+    style = vtk.vtkInteractorStyleTrackballCamera()
+    iren.SetInteractorStyle(style)
+   
+    global t1
+    global t2
+    global t3
+    t1 = iren.AddObserver ("LeftButtonPressEvent", pickerfunc)
+    t2 = iren.AddObserver ("LeftButtonReleaseEvent", pickerfunc)
+    t3 = iren.AddObserver ("RightButtonPressEvent", pickerfunc)
+
+    iren.Initialize()
+    iren.Start()
+
+if __name__ == "__main__":
+    main()
--- test/vtkPoint.py
+++ test/vtkPoint.py
@@ -3,9 +3,43 @@ __author__ = 'Su Lei'
 
 import vtk
 
+def reRender(obj, env):
+    print 'hhhh'
+    points1 = [
+        [0, 0.3, 0],
+        [0.1, 0, 0]
+    ]
+
+    vtk_points1 = vtk.vtkPoints()
+    vtk_points_cells1 = vtk.vtkCellArray()
+
+    vtk_points1.InsertPoint(0, points1[0])
+    vtk_points1.InsertPoint(1, points1[1])
+    vtk_points_cells1.InsertNextCell(2, [0, 1])
+
+    vtk_vertices1 = vtk.vtkPolyData()
+    vtk_vertices1.SetPoints(vtk_points1)
+    vtk_vertices1.SetVerts(vtk_points_cells1)
+
+    vtk_mapper1 = vtk.vtkPolyDataMapper()
+    vtk_mapper1.SetInputData(vtk_vertices1)
+
+    vtk_actor1 = vtk.vtkActor()
+    vtk_actor1.GetProperty().SetPointSize(10)
+    vtk_actor1.SetMapper(vtk_mapper1)
+
+    ren.AddActor(vtk_actor1)
+    renWin.Render()
+
+
+def test(obj, env):
+    print 'ssss'
+
+
+
 points = [
-    [0, 0, 0],
-    [1, 0, 0]
+    [0, 0.5, 0],
+    [0.4, 0, 0]
 ]
 
 vtk_points = vtk.vtkPoints()
@@ -28,11 +62,16 @@ vtk_actor.SetMapper(vtk_mapper)
 
 ren = vtk.vtkRenderer()
 renWin = vtk.vtkRenderWindow()
-iren = vtk.vtkRenderWindowInteractor()
+renWin.SetPointSmoothing(1)
 
+iren = vtk.vtkRenderWindowInteractor()
+iren.RemoveObservers('StartPickEvent')
+iren.AddObserver('StartPickEvent', test, 1.0)
 ren.AddActor(vtk_actor)
 renWin.AddRenderer(ren)
 iren.SetRenderWindow(renWin)
 
 renWin.Render()
+
+
 iren.Start()
